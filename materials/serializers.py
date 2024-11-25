1e57b75c71ad777b7d8c9f0d5cf2064388c8c3c2
Tikin686 <sergey.tikin94@gmail.com>
2024-11-25 18:32:35 +0300

    fix

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.validators import video_url_validator
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    subscription = serializers.SerializerMethodField()
+
+    def get_subscription(self, obj):
+        user = self.context["request"].user
+        return Subscription.objects.filter(user=user, course=obj).exists()
 
     def get_lessons_count(self, course):
         return course.lessons.count()
+
 
     class Meta:
         model = Course
@@ -15,6 +22,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    video_url = serializers.CharField(validators=[video_url_validator])
 
     class Meta:
         model = Lesson
